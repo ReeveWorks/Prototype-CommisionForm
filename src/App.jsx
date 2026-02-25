889a6b519558c932476d81ae59eb2c1e8d394815
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-02-25 10:30:07 +0800

    v0.7 Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,20 @@
 import { useState } from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import { togglemode } from './states/slices/darkmodeSlice'
+
 import Header from './components/header.jsx';
+import ArtistProducts from './components/artistProducts.jsx';
 import './styles/app.css'
 
 
 function App() {
   const darkmode = useSelector((state) => state.darkmode.darkmode);
-  const dispatch = useDispatch();
-
-  function toggleDarkmode() {
-    dispatch(togglemode(!darkmode.isDarkmode));
-  }
 
   return (
     <div className="app">
       <Header />
 
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>Dark Mode is <b>{darkmode.isDarkmode ? "Enabled" : "Disabled"}</b></p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
-      <p>---</p>
+      <ArtistProducts />
     </div>
   )
 }
