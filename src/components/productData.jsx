0c864ed2216035d721ee2e9fe34d18cdfb0d1903
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-02-28 17:02:09 +0800

    v1.0 Update

--- src/components/productData.jsx
+++ src/components/productData.jsx
@@ -3,6 +3,7 @@ import '../styles/product.css'
 
 /* Functions/Hooks */
 import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react';
 
 /* Redux Slice */
 import { addProduct, updateProduct } from '../states/slices/artistDataSlice'
@@ -12,18 +13,34 @@ function productData() {
     const artistData = useSelector((state) => state.artist.artist);
     const dispatch = useDispatch();
 
+    /* System */
+    const [prod, setprod] = useState({
+        name: "Chibi",
+        details: {},
+    });
+    function InputChange(event) {
+        setprod(event.target.value);
+    }
+
+
     return (
         <div>
             <br />
             <h1>Product Info</h1>
             <br />
-            <input className="prod-textbox" type="text" placeholder="Product Name" />
+            <input
+                className="prod-textbox"
+                type="text"
+                value={prod.name}
+                onChange={InputChange}
+                placeholder="Product Name" />
             <br />
-            <input className="prod-textbox" type="text" placeholder="Product Description"/>
-            <br />
-            <textarea className="prod-textbox" rows="5" cols="40">
-                text here
-            </textarea>
+            <textarea
+                className="prod-textbox"
+                rows="3"
+                cols="40"
+                placeholder="Product Description"/>
+            
         </div>
     );
 }
