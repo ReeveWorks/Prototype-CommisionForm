1ea6049644c9400e35d313c02d076e56509a8da2
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-03-24 09:20:17 +0800

    v3.4 Code

--- src/components/productData.jsx
+++ src/components/productData.jsx
@@ -18,8 +18,8 @@ function productData() {
         name: "Chibi",
         description: "A chibi reaction stickers to your liking!",
         module: [
-            { id: "stm-ttl01", type: "title", content: "Time to fill in your commission details!", size: "h1" },
-            { id: "inm-txt01", type: "text-input", content: "Number of stickers" },
+            { id: "stm-ttl01", type: "title", content: "Fill in your commission details!", size: "h3" },
+            { id: "inm-txt02", type: "text-input", content: "Any additional details?" },
         ],
     });
 
@@ -27,6 +27,68 @@ function productData() {
         setprod({ ...testProd, [key]: event.target.value });
     }
 
+    function handleModuleChange(index, key, value) {
+        const newModule = testProd.module.map((thisModule, idx) => {
+            if (idx !== index) return thisModule;
+            return { ...thisModule, [key]: value };
+        });
+        setprod({ ...testProd, module: newModule });
+    }
+
+
+    function renderModuleInput(moduleItem, index) {
+        switch (moduleItem.type) {
+            case 'title':
+                return (
+                    <div key={index} className="module-container">
+                        <div className='module-tab'>
+                            <span>ID: {moduleItem.id}</span>
+                            <a>✓</a>
+                            <a>↩</a>
+                            <a>✕</a>
+                        </div>
+                        <div className='module-options'>
+                            <i className='module-text'>Title</i>
+                            <input
+                                type='text'
+                                className='input-textbox'
+                                placeholder='text here!'
+                                value={moduleItem.content}
+                                onChange={(e) => handleModuleChange(index, 'content', e.target.value)} />
+                            <i className='module-text'>Size</i>
+                            <select
+                                className='input-textbox'
+                                value={moduleItem.size}
+                                onChange={(e) => handleModuleChange(index, 'size', e.target.value)}>
+                                <option value="h1">H1</option>
+                                <option value="h2">H2</option>
+                                <option value="h3">H3</option>
+                            </select>
+                        </div>
+                    </div>
+                );
+            case 'text-input':
+                return (
+                    <div key={index} className="module-container">
+                        <div className='module-tab'>
+                            <span>ID: {moduleItem.id}</span>
+                            <a>✓</a>
+                            <a>↩</a>
+                            <a>✕</a>
+                        </div>
+                        <div className='module-options'>
+                            <i className='module-text'>Textbox Name</i>
+                            <input
+                                type='text'
+                                className='input-textbox'
+                                placeholder='text here!'
+                                value={moduleItem.content}
+                                onChange={(e) => handleModuleChange(index, 'content', e.target.value)} />
+                        </div>
+                    </div>
+                );
+        }
+    }
 
     return (
         <div className='comp-prod'>
@@ -48,11 +110,11 @@ function productData() {
                 className="prod-textbox"
                 placeholder="Product Description" />
             <br />
-            
-            {/*  */}
 
             {/*  */}
-            
+            {testProd.module.map((m, idx) => renderModuleInput(m, idx))}
+            {/*  */}
+
             <br />
 
             {/* <button className='btn-add-module'>
