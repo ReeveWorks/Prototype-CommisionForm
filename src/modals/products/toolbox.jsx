e1a93f7ecb3ce7f073585c7bc8a9755411b7f786
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-06-09 12:20:02 +0800

    v9.2.7 Update

--- src/modals/products/toolbox.jsx
+++ src/modals/products/toolbox.jsx
@@ -1,11 +1,22 @@
 /* Stylesheets */
 import '../../styles/toolbox.css'
+import { useState } from 'react';
 
 function toolBox() {
-   
+    // dragElement(document.getElementById("tool-drag"));
+    const [dragElement] = useState(document.getElementById("tool-drag"));
+
+    function log(value) {
+        console.log(value);
+    }
+
     return (
-        <div id="tool-drag" className='toolbox-container'>
-            <p className='use-icon txt-unselectable'>T</p>
+        <div
+            id="tool-drag"
+            className='toolbox-container'
+            onMouseDown={() => log("down")}
+            onMouseUp={() => log("up")}>
+            <p className='use-icon txt-unselectable' >T</p>
         </div>
     );
 }
