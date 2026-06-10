86e6680e586814ef9fae6cc8bf81c5686f435e0a
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-06-10 11:09:46 +0800

    v9.2.8 Update
    Target:
    - Tool Box [ 7% ]
    - Create Add function [ 50% ]
    
    Next Target:
    - Icon font file [ 15% ]
    - Function: Live Calculation of Commission
    - Dark/Light Icon Change

--- src/modals/products/toolbox.jsx
+++ src/modals/products/toolbox.jsx
@@ -3,19 +3,21 @@ import '../../styles/toolbox.css'
 import { useState } from 'react';
 
 function toolBox() {
-    // dragElement(document.getElementById("tool-drag"));
     const [dragElement] = useState(document.getElementById("tool-drag"));
 
-    function log(value) {
-        console.log(value);
+    function log(value, e) {
+        e = e || window.event;
+        e.preventDefault();
+        
+        console.log(`${value} button\nx-axis: ${e.clientX}\ny-axis: ${e.clientY}`);
     }
 
     return (
         <div
             id="tool-drag"
             className='toolbox-container'
-            onMouseDown={() => log("down")}
-            onMouseUp={() => log("up")}>
+            onMouseDown={(e) => log("down", e)}
+            onMouseUp={(e) => log("up", e)}>
             <p className='use-icon txt-unselectable' >T</p>
         </div>
     );
