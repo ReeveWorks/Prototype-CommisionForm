33caddba4571b20d0a1347ccfbb2f39ea6e5443b
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-06-11 10:09:39 +0800

    v9.2.9 Update
    Target:
    - Tool Box [ 8% ]
    - Create Add function [ 50% ]
    
    Next Target:
    - Icon font file [ 15% ]
    - Function: Live Calculation of Commission
    - Dark/Light Icon Change

--- src/modals/products/toolbox.jsx
+++ src/modals/products/toolbox.jsx
@@ -1,19 +1,41 @@
 /* Stylesheets */
 import '../../styles/toolbox.css'
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function toolBox() {
-    const [dragElement] = useState(document.getElementById("tool-drag"));
+    const dragElementRef = useRef(null);
+    const [toolPosition, setToolPosition] = useState({ x: 0, y: 0 });
 
     function log(value, e) {
         e = e || window.event;
         e.preventDefault();
+
+        const target = e.currentTarget || dragElementRef.current;
         
         console.log(`${value} button\nx-axis: ${e.clientX}\ny-axis: ${e.clientY}`);
+
+        if (value === "down") {
+            target.style.color = 'red';
+        } else if (value === "up") {
+            target.style.color = 'white';
+        }
+    
+
+        target.style.top = `${(target.offsetTop)}px`;
+        target.style.left = `${(target.offsetLeft)}px`;
     }
 
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setCount((count) => count + 1);
+    //         console.log('count updated:', count);
+    //     }, 1000);
+    // });
+
+    // onMouseMove={(e) => log("move", e)}   
     return (
         <div
+            ref={dragElementRef}
             id="tool-drag"
             className='toolbox-container'
             onMouseDown={(e) => log("down", e)}
@@ -22,5 +44,4 @@ function toolBox() {
         </div>
     );
 }
-
 export default toolBox;
