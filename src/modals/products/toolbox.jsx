73b3f8883e6f6f83659a02d8b4e0880856b5674b
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-06-14 14:27:13 +0800

    v9.2.12 Update

--- src/modals/products/toolbox.jsx
+++ src/modals/products/toolbox.jsx
@@ -8,6 +8,24 @@ function toolBox() {
     const [toolmove, setToolMove] = useState(false);
     const [tooldiv, setToolDiv] = useState(null);
 
+    const tooldivRef = useRef({
+        isDragging: false,
+        mouseX: 0,
+        mouseY: 0,
+        moveX: 0,
+        moveY: 0,
+    });
+
+    function mouseDown(e) {
+        console.log(`X-axis: ${e.clientX}, Y-axis: ${e.clientY}`);
+
+        tooldivRef.current = {
+            isDragging: true,
+            mouseX: e.clientX,
+            mouseY: e.clientY
+        };
+    }
+
     function log(value, e) {
         if (value === "down") {
             tooldiv.style.color = 'red';
@@ -21,24 +39,32 @@ function toolBox() {
         setToolDiv(e.currentTarget || dragElementRef.current);
 
         if (value === "move" && toolmove) {
-            tooldiv.style.top = 
-            `${(tooldiv.offsetTop - (pos.y - e.clientY))}px`;
-            tooldiv.style.left = 
-            `${(tooldiv.offsetLeft - (pos.x - e.clientX))}px`;
+            tooldiv.style.top =
+                `${(tooldiv.offsetTop - (pos.y - e.clientY))}px`;
+            tooldiv.style.left =
+                `${(tooldiv.offsetLeft - (pos.x - e.clientX))}px`;
         }
 
         setpos({ x: e.clientX, y: e.clientY, });
     }
+
+    // <div
+    //     ref={dragElementRef}
+    //     id="tool-drag"
+    //     className='toolbox-container'
+    //     onMouseDown={(e) => log("down", e)}
+    //     onMouseUp={(e) => log("up", e)}
+    //     onMouseMove={(e) => log("move", e)}
+    //     onMouseLeave={(e) => log("move", e)}>
+    //     <p className='use-icon txt-unselectable' >T</p>
+    // </div>
 
     return (
         <div
             ref={dragElementRef}
             id="tool-drag"
             className='toolbox-container'
-            onMouseDown={(e) => log("down", e)}
-            onMouseUp={(e) => log("up", e)}
-            onMouseMove={(e) => log("move", e)}
-            onMouseLeave={(e) => log("move", e)}>
+            onMouseDown={(e) => log("down", e)}>
             <p className='use-icon txt-unselectable' >T</p>
         </div>
     );
